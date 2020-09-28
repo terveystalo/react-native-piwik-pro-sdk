5a98762884d5c89f800fe9fc4f8d5ba08afd3911
Axel Eirola <axel.eirola@terveystalo.com>
2020-09-28 15:55:05 +0300

    test: missing parameters

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,43 @@
-it.todo('write a test');
+jest.mock('react-native', () => ({
+  NativeModules: {
+    PiwikProSdk: {
+      init: jest.fn(),
+      trackScreen: jest.fn(),
+      trackEvent: jest.fn(),
+      dispatch: jest.fn(),
+    },
+  },
+}));
+
+import { NativeModules } from 'react-native';
+
+import * as PiwikProSdk from '../';
+
+describe('react-native-piwik-pro-sdk', () => {
+  describe('init', () => {
+    it('adds missing parameters', async () => {
+      await PiwikProSdk.init('baseUrl', 'siteId');
+
+      expect(NativeModules.PiwikProSdk.init).toHaveBeenCalledWith(
+        'baseUrl',
+        'siteId',
+        {}
+      );
+    });
+  });
+
+  describe('trackEvent', () => {
+    it('adds missing parameters', async () => {
+      await PiwikProSdk.trackEvent('category', 'action');
+
+      expect(NativeModules.PiwikProSdk.trackEvent).toHaveBeenCalledWith(
+        'category',
+        'action',
+        {
+          name: undefined,
+          value: undefined,
+        }
+      );
+    });
+  });
+});
