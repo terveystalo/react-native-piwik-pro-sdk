894a7434b95082ce64545e30fd5b5e946a63b9a7
Jani Kinnunen <jani.kinnunen1@gmail.com>
2020-11-24 11:52:27 +0200

    test: add custom dimensions and events for testing

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   const [result, setResult] = React.useState<{ type: string; error?: Error }>({
     type: 'Loading',
   });
+
   React.useEffect(() => {
     (async () => {
       // Initialize the SDK
@@ -13,10 +14,19 @@ export default function App() {
         'https://demoaccess.piwik.pro/',
         '3e7e6ab9-d605-42b0-ac1b-cdf5bb5e216f'
       );
+
+      // Set custom dimension with action scope. Action scope will limit the custom dimension to a single action.
+      await PiwikProSdk.setCustomDimension(1, 'first', 'action');
       // Track screen view
       await PiwikProSdk.trackScreen('main/list');
       // Track custom event
       await PiwikProSdk.trackEvent('app', 'launch', 'notification', 1.04);
+      // Set custom dimension with visit scope. Using visit scope will associate the custom dimension with the current session.
+      await PiwikProSdk.setCustomDimension(2, 'second', 'visit');
+      await PiwikProSdk.trackEvent('app', 'test1');
+      // Another action scope will replace the previous one.
+      await PiwikProSdk.setCustomDimension(3, 'third', 'action');
+      await PiwikProSdk.trackEvent('app', 'test2');
       // Immediately dispatch all events
       await PiwikProSdk.dispatch();
     })().then(
